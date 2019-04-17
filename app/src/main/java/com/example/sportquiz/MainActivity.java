664429212014bae3221eb8800689288ffd47b107
Mohamed Abdelrazik <mohamedabdelrazik188@gmail.com>
2019-04-17 17:19:13 +0200

    commit Lap and hub

--- app/src/main/java/com/example/sportquiz/MainActivity.java
+++ app/src/main/java/com/example/sportquiz/MainActivity.java
@@ -30,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
     RatingBar rating;
     String lang , answer;
 
-    // first commit in sport quiz
-    // second commit in sport quiz
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
